fde6fad67d6fc131c2a0167683ad2049e290474d
updaun <updauney@daum.net>
2024-06-08 10:46:58 +0900

    [feat] add exception handler

--- src/shared/urls.py
+++ src/shared/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.urls import path
 from ninja import NinjaAPI
 from user.authentication import bearer_auth
+from user.exceptions import NotAuthorizedException, UserNotFoundException
 
 
 base_api = NinjaAPI(title="Goodpang", version="0.0.0")
@@ -35,6 +36,24 @@ def auth_test(request):
         "token": request.auth,
         "email": request.user.email,
     }
+
+
+@base_api.exception_handler(NotAuthorizedException)
+def not_authorized_exception(request, exc):
+    return base_api.create_response(
+        request,
+        {"results": {"message": exc.message}},
+        status=401,
+    )
+
+
+@base_api.exception_handler(UserNotFoundException)
+def user_not_found_exception(request, exc):
+    return base_api.create_response(
+        request,
+        {"results": {"message": exc.message}},
+        status=404,
+    )
 
 
 urlpatterns = [
