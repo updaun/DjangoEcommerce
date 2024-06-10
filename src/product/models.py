21d921ce51f4c6b5695f785e14fac3bfd3993871
updaun <updauney@daum.net>
2024-06-10 12:03:44 +0900

    [feat] full text search

--- src/product/models.py
+++ src/product/models.py
@@ -1,5 +1,7 @@
 from enum import StrEnum
 from django.db import models
+from django.contrib.postgres.search import SearchVectorField
+from django.contrib.postgres.indexes import GinIndex
 
 
 class ProductStatus(StrEnum):
@@ -14,12 +16,15 @@ class Product(models.Model):
     # is_active = models.BooleanField(default=False)
     status = models.CharField(max_length=8)  # active | inactive | paused
     category = models.ForeignKey("Category", on_delete=models.SET_NULL, null=True)
+    tags = models.CharField(max_length=128, blank=True)
+    search_vector = SearchVectorField(null=True)
 
     class Meta:
         app_label = "product"
         db_table = "product"
         indexes = [
             models.Index(fields=["status", "price"]),
+            GinIndex(fields=["search_vector"]),
         ]
 
 
--- src/product/urls.py
+++ src/product/urls.py
@@ -1,5 +1,6 @@
 from typing import List
 from django.http import HttpRequest
+from django.contrib.postgres.search import SearchQuery
 from ninja import Router
 
 from product.response import CategoryListResponse, ProductListResponse
@@ -14,8 +15,14 @@ router = Router(tags=["Products"])
     "",
     response={200: ObjectResponse[ProductListResponse]},
 )
-def product_list_handler(request: HttpRequest, category_id: int | None = None):
-    if category_id:
+def product_list_handler(
+    request: HttpRequest, category_id: int | None = None, query: str | None = None
+):
+    if query:
+        products = Product.objects.filter(
+            search_vector=SearchQuery(query), status=ProductStatus.ACTIVE
+        ).values("id", "name", "price")
+    elif category_id:
         category: Category | None = Category.objects.filter(id=category_id).first()
         if not category:
             products = []
